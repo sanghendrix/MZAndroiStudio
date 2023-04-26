1794530e207e37be72feaa615b7f3d89131621b9
sanghendrix <37829642+sanghendrix@users.noreply.github.com>
2023-04-26 16:32:04 +0700

    Add files via upload

--- CustomArmorEquip.js
+++ CustomArmorEquip.js
@@ -1,148 +1,235 @@
 /*:
- * @plugindesc Custom Equip Scene
- * @author ChatGPT
+ * @target MZ
+ * @plugindesc Custom Armor Equip
+ * @author YourName
  *
- * @param targetActorId
- * @text Target Actor ID
- * @desc The ID of the actor to display and manage equipment for.
+ * @param ActorId
+ * @text Actor ID
+ * @desc ID of the actor for which the armors will be equipped.
  * @type number
- * @min 1
  * @default 1
  *
- * @param helpWindowX
- * @text Help Window X
- * @desc The X position of the help window. Use "center" to center it horizontally.
+ * @param CategoryWindowPosition
+ * @text Category Window Position
+ * @desc Position of the category window. Choose 'above' to place it above the item window, 'below' to place it below.
  * @type combo
- * @option center
- * @default center
+ * @option above
+ * @option below
+ * @default above
  *
- * @param helpWindowY
- * @text Help Window Y
- * @desc The Y position of the help window. Use "center" to center it vertically.
- * @type combo
- * @option center
- * @default center
- *
- * @param helpWindowWidth
- * @text Help Window Width
- * @desc The width of the help window.
+ * @param WindowWidth
+ * @text Window Width
+ * @desc Width of the item and help windows.
  * @type number
- * @min 1
  * @default 816
  *
- * @param helpWindowHeight
+ * @param WindowHeight
+ * @text Window Height
+ * @desc Height of the item window.
+ * @type number
+ * @default 456
+ *
+ * @param WindowX
+ * @text Window X
+ * @desc X position of the item and help windows. Use 'center' to center the windows horizontally.
+ * @type combo
+ * @option center
+ * @default center
+ *
+ * @param WindowY
+ * @text Window Y
+ * @desc Y position of the item and help windows. Use 'center' to center the windows vertically.
+ * @type combo
+ * @option center
+ * @default center
+ *
+ * @param HelpWindowHeight
  * @text Help Window Height
- * @desc The height of the help window.
+ * @desc Height of the help window.
  * @type number
- * @min 1
  * @default 72
  *
- * @param armorWindowX
- * @text Armor Window X
- * @desc The X position of the armor window. Use "center" to center it horizontally.
+ * @param HelpWindowX
+ * @text Help Window X
+ * @desc X position of the help window. Use 'center' to center the window horizontally.
  * @type combo
  * @option center
  * @default center
  *
- * @param armorWindowY
- * @text Armor Window Y
- * @desc The Y position of the armor window. Use "center" to center it vertically.
+ * @param HelpWindowY
+ * @text Help Window Y
+ * @desc Y position of the help window. Use 'center' to center the window vertically.
  * @type combo
  * @option center
+ * @option above
+ * @option below
  * @default center
  *
- * @param armorWindowWidth
- * @text Armor Window Width
- * @desc The width of the armor window.
+ * @param HelpWindowWidth
+ * @text Help Window Width
+ * @desc Width of the help window.
  * @type number
- * @min 1
  * @default 816
  *
- * @param armorWindowHeight
- * @text Armor Window Height
- * @desc The height of the armor window.
+ * @param HelpWindowHeight
+ * @text Help Window Height
+ * @desc Height of the help window.
  * @type number
- * @min 1
- * @default 408
- *
- * @param categoryWindowHeight
- * @text Category Window Height
- * @desc The height of the category window.
- * @type number
- * @min 1
  * @default 72
- *
- * @param statusWindowX
+ * 
+ * @param StatusWindowX
  * @text Status Window X
- * @desc The X position of the status window. Use "center" to center it horizontally.
+ * @desc X position of the status window. Use 'center' to center the window horizontally.
  * @type combo
  * @option center
- * @default 0
- *
- * @param statusWindowY
+ * @default center
+ * 
+ * @param StatusWindowY
  * @text Status Window Y
- * @desc The Y position of the status window. Use "center" to center it vertically.
+ * @desc Y position of the status window. Use 'center' to center the window vertically.
  * @type combo
  * @option center
- * @default 0
- *
- * @param statusWindowWidth
+ * @default center
+ * 
+ * @param StatusWindowWidth
  * @text Status Window Width
- * @desc The width of the status window.
+ * @desc Width of the status window.
  * @type number
- * @min 1
- * @default 300
- *
- * @param statusWindowHeight
+ * @default 816
+ * 
+ * @param StatusWindowHeight
  * @text Status Window Height
- * @desc The height of the status window.
+ * @desc Height of the status window.
  * @type number
- * @min 1
- * @default 144
+ * @default 72
+ * 
+ * @param Actor Image
+ * @type file
+ * @dir img/system
+ * @desc The actor image to be displayed in the Equip Scene. Place the image in the img/system folder.
+ * @default Actor1_1
  *
- * @param paramName
- * @text Parameter Name
- * @desc The text to display for the first parameter in the status window.
- * @type string
- * @default Weapon
+ * @param Actor Image Position
+ * @type struct<ImagePosition>
+ * @desc Position and size settings for the actor image.
+ * @default {"x":"0","y":"0","size":"1.0"}
  *
- * @param paramName2
- * @text Parameter Name2
- * @desc The text to display for the second parameter in the status window.
- * @type string
- * @default Body Armor
- * @help This plugin allows you to create a custom equip scene that only displays armors.
+ * @help CustomArmorEquip.js
+ *
+ * This plugin modifies the default equip scene to only show armors in a single window.
+ * When an armor is triggered, it will be automatically equipped to the specified actor.
  */
+/*~struct~ImagePosition:
+ * @param x
+ * @type number
+ * @min -9999
+ * @max 9999
+ * @default 0
+ * @desc X position of the actor image.
+ *
+ * @param y
+ * @type number
+ * @min -9999
+ * @max 9999
+ * @default 0
+ * @desc Y position of the actor image.
+ *
+ * @param size
+ * @type number
+ * @decimals 2
+ * @min 0
+ * @max 10
+ * @default 1.0
+ * @desc The size (scale) of the actor image. Maintains aspect ratio.
+ */
+
 (() => {
-  const pluginName = "CustomEquipScene";
+  const pluginName = "CustomArmorEquip";
   const parameters = PluginManager.parameters(pluginName);
+  const targetActorId = parseInt(parameters["ActorId"]) || 1;
+  const categoryWindowPosition = parameters["CategoryWindowPosition"] || "above";
+  const windowWidth = parseInt(parameters["WindowWidth"]) || 816;
+  const windowHeight = parseInt(parameters["WindowHeight"]) || 456;
+  const windowX = parameters["WindowX"];
+  const windowY = parameters["WindowY"];
+  const helpWindowHeight = parseInt(parameters["HelpWindowHeight"]) || 72;
+  const helpWindowX = parameters["HelpWindowX"];
+  const helpWindowY = parameters["HelpWindowY"];
+  const helpWindowWidth = parseInt(parameters["HelpWindowWidth"]) || 816;
+  const statusWindowX = parameters["StatusWindowX"];
+  const statusWindowY = parameters["StatusWindowY"];
+  const statusWindowWidth = parseInt(parameters["StatusWindowWidth"]) || 816;
+  const statusWindowHeight = parseInt(parameters["StatusWindowHeight"]) || 72;
+  const actorImage = String(parameters["Actor Image"] || "Actor1_1");
+const actorImageX = String(parameters["Actor Image X"] || "0");
+const actorImageY = String(parameters["Actor Image Y"] || "0");
+const actorImageSize = parseFloat(parameters["Actor Image Size"] || "1.0");
 
-  const targetActorId = Number(parameters["targetActorId"] || 1);
-  const helpWindowX = (parameters["helpWindowX"] || "center").toLowerCase();
-  const helpWindowY = (parameters["helpWindowY"] || "center").toLowerCase();
-  const helpWindowWidth = Number(parameters["helpWindowWidth"]);
-  const helpWindowHeight = Number(parameters["helpWindowHeight"]);
-  const armorWindowX = (parameters["armorWindowX"] || "center").toLowerCase();
-  const armorWindowY = (parameters["armorWindowY"] || "center").toLowerCase();
-  const armorWindowWidth = Number(parameters["armorWindowWidth"]);
-  const armorWindowHeight = Number(parameters["armorWindowHeight"]);
-  const categoryWindowHeight = Number(parameters["categoryWindowHeight"]);
-  const statusWindowX = (parameters["statusWindowX"] || "center").toLowerCase();
-  const statusWindowY = (parameters["statusWindowY"] || "center").toLowerCase();
-  const statusWindowWidth = Number(parameters["statusWindowWidth"]);
-  const statusWindowHeight = Number(parameters["statusWindowHeight"]);
-  const paramName = parameters["paramName"];
-  const paramName2 = parameters["paramName2"];
 
-  Scene_Equip.prototype.create = function () {
-    Scene_ItemBase.prototype.create.call(this);
-    this.createHelpWindow();
-    this.createItemWindow();
-    this.createStatusWindow();
-    this.createCommandWindow();
-    this.refreshActor();
+  class Window_CustomEquipCategory extends Window_HorzCommand {
+    initialize(rect) {
+      super.initialize(rect);
+      this.select(0);
+      this.callHandler("ok");
+    }
+
+    makeCommandList() {
+      this.addCommand("Weapon", "weapon");
+      this.addCommand("Armor", "armor");
+    }
+  }
+
+
+  Window_CustomEquipCategory.prototype.setItemWindow = function (itemWindow) {
+    this._itemWindow = itemWindow;
+    this.update();
   };
 
+  Scene_Equip.prototype.create = function () {
+    Scene_MenuBase.prototype.create.call(this);
+    this.createHelpWindow();
+    this.createCategoryWindow();
+    this.createItemWindow();
+    this.createStatusWindow();
+    this.createActorImage();
+  };
+
+  Scene_Equip.prototype.createActorImage = function () {
+    const bitmap = ImageManager.loadSystem(actorImage);
+    const sprite = new Sprite(bitmap);
+    sprite.x = parseInt(actorImageX);
+    sprite.y = parseInt(actorImageY);
+    sprite.scale.x = actorImageSize;
+    sprite.scale.y = actorImageSize;
+    this.addChild(sprite);
+  };
+
+  Scene_Equip.prototype.createCategoryWindow = function () {
+    const rect = new Rectangle(0, 0, windowWidth, helpWindowHeight);
+    if (windowX === "center") {
+      rect.x = (Graphics.boxWidth - rect.width) / 2;
+    } else {
+      rect.x = parseInt(windowX) || 0;
+    }
+    if (categoryWindowPosition === "above") {
+      if (windowY === "center") {
+        rect.y = (Graphics.boxHeight - windowHeight - rect.height) / 2;
+      } else {
+        rect.y = parseInt(windowY) - rect.height;
+      }
+    } else { // below
+      if (windowY === "center") {
+        rect.y = (Graphics.boxHeight + windowHeight) / 2;
+      } else {
+        rect.y = parseInt(windowY) + windowHeight;
+      }
+    }
+    this._categoryWindow = new Window_CustomEquipCategory(rect);
+    this._categoryWindow.setHandler("ok", this.onCategoryOk.bind(this));
+    this._categoryWindow.setHandler("cancel", this.popScene.bind(this));
+    this.addWindow(this._categoryWindow);
+  };
+  
   Scene_Equip.prototype.createHelpWindow = function () {
     const rect = new Rectangle(0, 0, helpWindowWidth, helpWindowHeight);
     if (helpWindowX === "center") {
@@ -150,14 +237,53 @@
     } else {
       rect.x = parseInt(helpWindowX) || 0;
     }
-    if (helpWindowY === "center") {
+  
+    if (helpWindowY === "above") {
+      if (categoryWindowPosition === "above") {
+        rect.y = parseInt(windowY) - helpWindowHeight * 2;
+      } else {
+        rect.y = parseInt(windowY) - helpWindowHeight;
+      }
+    } else if (helpWindowY === "below") {
+      if (categoryWindowPosition === "above") {
+        rect.y = parseInt(windowY) + windowHeight;
+      } else {
+        rect.y = parseInt(windowY) + windowHeight + helpWindowHeight;
+      }
+    } else if (helpWindowY === "center") {
       rect.y = (Graphics.boxHeight - rect.height) / 2;
     } else {
       rect.y = parseInt(helpWindowY) || 0;
     }
+  
     this._helpWindow = new Window_Help(rect);
     this.addWindow(this._helpWindow);
   };
+  
+  Scene_Equip.prototype.createItemWindow = function () {
+    const rect = new Rectangle(0, 0, windowWidth, windowHeight);
+    if (windowX === "center") {
+      rect.x = (Graphics.boxWidth - rect.width) / 2;
+    } else {
+      rect.x = parseInt(windowX) || 0;
+    }
+    if (windowY === "center") {
+      rect.y = (Graphics.boxHeight - rect.height) / 2;
+    } else {
+      rect.y = parseInt(windowY) || 0;
+    }
+    if (categoryWindowPosition === "above") {
+      rect.y += helpWindowHeight;
+    }
+    this._itemWindow = new Window_EquipItem(rect);
+    this._itemWindow.setHelpWindow(this._helpWindow);
+    this._itemWindow.setHandler("ok", this.onItemOk.bind(this));
+    this._itemWindow.setHandler("cancel", this.onItemCancel.bind(this));
+    this.addWindow(this._itemWindow);
+    this._categoryWindow.setItemWindow(this._itemWindow);
+  };
+  
+  
 
   Scene_Equip.prototype.createStatusWindow = function () {
     const rect = new Rectangle(0, 0, statusWindowWidth, statusWindowHeight);
@@ -175,45 +301,6 @@
     this.addWindow(this._statusWindow);
   };
 
-  Scene_Equip.prototype.createCommandWindow = function () {
-    const rect = new Rectangle(0, this._helpWindow.height, this._itemWindow.width, categoryWindowHeight);
-    this._categoryWindow = new Window_CustomEquipCategory(rect);
-    this._categoryWindow.setHandler("ok", this.onCategoryOk.bind(this));
-    this._categoryWindow.setHandler("cancel", this.popScene.bind(this));
-    this.addWindow(this._categoryWindow);
-  };
-
-  Scene_Equip.prototype.createItemWindow = function () {
-    const armorWindowWidth = parseInt(parameters["armorWindowWidth"]) || 240;
-    const armorWindowHeight = parseInt(parameters["armorWindowHeight"]) || 180;
-    const armorWindowX = (parameters["armorWindowX"] || "center").toLowerCase();
-    const armorWindowY = (parameters["armorWindowY"] || "center").toLowerCase();
-    const rect = new Rectangle();
-  
-    rect.width = armorWindowWidth;
-    rect.height = armorWindowHeight;
-  
-    if (armorWindowX === "center") {
-      rect.x = (Graphics.boxWidth - rect.width) / 2;
-    } else {
-      rect.x = parseInt(armorWindowX) || 0;
-    }
-  
-    if (armorWindowY === "center") {
-      rect.y = (Graphics.boxHeight - rect.height) / 2;
-    } else {
-      rect.y = parseInt(armorWindowY) || 0;
-    }
-  
-    this._itemWindow = new Window_EquipItem(rect);
-    this._itemWindow.setHelpWindow(this._helpWindow);
-    this._itemWindow.setHandler("ok", this.onItemOk.bind(this));
-    this._itemWindow.setHandler("cancel", this.onItemCancel.bind(this));
-    this._categoryWindow.setItemWindow(this._itemWindow);
-    this.addWindow(this._itemWindow);
-  };
-  
-
   Scene_Equip.prototype.onCategoryOk = function () {
     const category = this._categoryWindow.currentSymbol();
     this._itemWindow.setCategory(category);
@@ -221,53 +308,21 @@
     this._itemWindow.select(0);
   };
 
+
   Scene_Equip.prototype.onItemOk = function () {
-    SoundManager.playEquip();
-    $gameActors.actor(targetActorId).changeEquipById(this._categoryWindow.index(), this._itemWindow.item().id);
+    const item = this._itemWindow.item();
+    if (item && $gameActors.actor(targetActorId).canEquip(item)) {
+      $gameActors.actor(targetActorId).changeEquip(item.etypeId - 1, item);
+    }
     this._itemWindow.refresh();
     this._statusWindow.refresh();
     this._itemWindow.activate();
   };
 
-  class Window_CustomEquipCategory extends Window_HorzCommand {
-    initialize(rect) {
-      super.initialize(rect);
-      this.select(0);
-      this.callHandler("ok");
-    }
-    makeCommandList() {
-      this.addCommand("Weapon", "weapon");
-      this.addCommand("Armor", "armor");
-    }
-  }
-
-  class Window_EquipStatus extends Window_Base {
-    constructor(rect) {
-      super(rect);
-      this.refresh();
-    }
-    refresh() {
-      this.contents.clear();
-      const actor = $gameActors.actor(targetActorId);
-      if (!actor) return;
-
-      const name = actor.name();
-      const weapon = actor.weapons()[0];
-      const bodyArmor = actor.armors().find((armor) => armor.atypeId === 1);
-
-      this.drawText(name, 0, 0);
-      this.drawText(paramName, 0, this.lineHeight());
-      if (weapon) {
-        this.drawIcon(weapon.iconIndex, 0, this.lineHeight() * 2);
-        this.drawText(weapon.name, this.iconWidth() + 2, this.lineHeight() * 2);
-      }
-      this.drawText(paramName2, 0, this.lineHeight() * 3);
-      if (bodyArmor) {
-        this.drawIcon(bodyArmor.iconIndex, 0, this.lineHeight() * 4);
-        this.drawText(bodyArmor.name, this.iconWidth() + 2, this.lineHeight() * 4);
-      }
-    }
-  }
+  Scene_Equip.prototype.onItemCancel = function () {
+    this._itemWindow.deselect();
+    this._categoryWindow.activate();
+  };
 
   class Window_EquipItem extends Window_ItemList {
     setCategory(category) {
@@ -275,6 +330,7 @@
       this.refresh();
       this.resetScroll();
     }
+
     makeItemList() {
       if (this._category === "armor") {
         this._data = $gameParty.armors();
@@ -289,4 +345,24 @@
       return $gameActors.actor(targetActorId).canEquip(item);
     }
   }
+
+
+  class Window_EquipStatus extends Window_Base {
+    initialize(rect) {
+      super.initialize(rect);
+      this.refresh();
+    }
+
+    refresh() {
+      const actor = $gameActors.actor(targetActorId);
+      this.contents.clear();
+      if (actor) {
+        const lineHeight = this.lineHeight();
+        this.drawActorName(actor, 0, 0);
+        this.drawItemName(actor.equips()[0], 0, lineHeight * 1);
+        this.drawItemName(actor.equips()[1], 0, lineHeight * 2);
+      }
+    }
+  }
 })();
+
